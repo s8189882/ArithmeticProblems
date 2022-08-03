1d8c4f2ec30cc491e9fbea30f73c6e940fa1d76f
s8189882 <s8189882@gmail.com>
2022-08-03 17:31:38 +0530

    Computing a * b + c

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -25,3 +25,9 @@ operation1=$((n1+$((n2*$n3))))
 
 echo "Computation 1 :     a + b * c = $operation1"
 echo ""
+
+#Compute a * b + c  
+operation2=$(($((n1*$n2))+$n3))
+
+echo "Computation 2 :     a * b + c = $operation2"
+echo ""

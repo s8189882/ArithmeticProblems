0edbb97200cd511b26be94682262c59d73191983
s8189882 <s8189882@gmail.com>
2022-08-03 17:05:45 +0530

    Displaying welcome message.

---
+++ arithmeticComputationSorting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#
+#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
+#
+
+echo ""
+echo "Welcome to the Arithmetic Computation and Sorting Program"
+echo "This program computes different arithmetic expressions and sorts the results."
+echo ""
